f436da7d329edc76d8dbaf52bb80365373c03101
homaander <hom.ander23@gmail.com>
2019-08-03 21:24:08 +0300

    litle node

--- js/nodejs/syntax.js
+++ js/nodejs/syntax.js
@@ -8,6 +8,18 @@ const fs = require('fs');
 
 fs.unlink()
 
+// Эвенты
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+
+myEmitter.on('event', () => {
+  console.log('an event occurred!');
+});
+
+myEmitter.emit('event');
 
 // Чтение строки
 const readline = require('readline');
