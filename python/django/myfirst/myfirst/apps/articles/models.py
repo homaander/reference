3b6b9bf652c4d91d963749149011a884290ae0aa
homaander <hom.ander23@gmail.com>
2019-08-28 17:49:29 +0300

    ok

--- python/django/myfirst/myfirst/apps/articles/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Article(models.Model):
-    title = models.CharField('Название стстьи', max_lenght = 200)
-    text  = models.TextField('Текст статьи')
-    date  = models.DateTimeField('Дата публикации')
