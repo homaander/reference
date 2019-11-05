4b98a18ae3557971bec5de98e16ec6451c73e33b
homaander <hom.ander23@gmail.com>
2019-11-05 23:07:32 +0300

    ...
    New JS

---
+++ js/jQuery.js
@@ -0,0 +1,87 @@
+$(document).ready(() => {});
+$(() => {});
+
+// CSS Селекторы
+let foo = $('.class');
+let foo = $('#id');
+
+
+$(this).html('new html');
+$(this).css('color', 'green');
+$(this).val('new value');
+
+
+$(this).addClass('new');
+$(this).removeClass('new');
+
+$(this).parent();
+$(this).parents();
+
+$(this).children('li');
+
+$(this).closest('ul');
+
+$(this).find('h1');
+
+$(this).prev();
+$(this).prev('h3');
+
+$(this).next();
+$(this).next('h3');
+
+$(this).siblings();
+$(this).siblings('.selected');
+
+$(this).hide();
+$(this).show();
+
+
+//
+	//
+		$('#button').on('click', function () {})
+
+	//
+		$(document).on(
+			'click',
+			'input[type="button"][value!="+"]', 
+			function () { $(this).html('ok'); }
+		);
+
+$('p:last')
+	//
+		.clone()
+	//
+		.insertAfter('p:last');
+
+// Удалить элемент
+	$('p:last').remove();
+
+// Загрузить данные из ссылки
+	$('#id').load('test.php .block ul');
+
+// Отправить форму
+	$('#form').submit();
+
+
+// Cookie
+	// Создать
+		$.cookie('name', 'value', { expires: 7, path: '/' });
+	// Читать
+		let foo = $.cookie('name');
+	// Удалить
+		$.cookie('name', null);
+
+
+// AJAX
+	$.ajax({
+	    url: 'app.php',
+	    method: 'post',
+	    data: {
+	      	name: 'Andrew',
+	      	age: 17
+	    },
+
+	    success: function(result) {
+	      $('#weather-temp').html('<strong>' + result + '</strong>');
+	    }
+	});
