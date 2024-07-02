d4812f93c8a8e67b7b2a0cb32f40ff1de3e48280
Andrew <51619347+homaander@users.noreply.github.com>
2024-07-02 18:41:30 +0300

    rebase

--- haskell/extra/aesontets/app/Main.hs
+++ haskell/extra/aesontets/app/Main.hs
@@ -5,51 +5,34 @@ import Data.Aeson
 import qualified Data.Text as T
 import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Lazy.Char8 as BC
-import GHC.Generics
 
--- import Lib
+import Lib
 
-data Book = Book
-  { title  :: T.Text
-  , author :: T.Text
-  , year   :: Int }
-  deriving (Show, Generic)
-
-data ErrorMesage = ErrorMesage 
-  { message   :: T.Text
-  , errorCode :: Int }
-  deriving Show
-
-instance FromJSON Book
-instance ToJSON Book
-
-instance FromJSON ErrorMesage where
-  parseJSON (Object v) = ErrorMesage <$> v .: "message" <*>  v .: "error"
-  parseJSON _ = error "123"
-
-instance ToJSON ErrorMesage where
-  toJSON (ErrorMesage msg errCode) = object
-    [ "message" .= msg
-    , "error"   .= errCode ]
-
+-- >>> myBook
+-- Book {title = "A", author = "B", year = 2024}
 myBook :: Book
 myBook =  Book {title = "A", author = "B", year = 2024}
 
+-- >>> myBookJSON
+-- >>> myErrorJSON
+-- "{\"author\":\"B\",\"title\":\"A\",\"year\":2024}"
+-- "{\"message\":\"Err\",\"error\":11}"
 myBookJSON, myErrorJSON :: B.ByteString
-
 myBookJSON  = encode myBook
 myErrorJSON = "{\"message\":\"Err\",\"error\":11}"
 
+-- >>> myJSONToErrorMessage
+-- Just (ErrorMesage {message = "Err", errorCode = 11})
 myJSONToErrorMessage :: Maybe ErrorMesage
 myJSONToErrorMessage =  decode myErrorJSON
 
 writeBook :: IO ()
 writeBook = do
-  B.writeFile "book.json" myBookJSON
+  B.writeFile "data/book.json" myBookJSON
 
 readBook :: IO ()
 readBook = do
-  jsonText <- B.readFile "book.json"
+  jsonText <- B.readFile "data/book.json"
   let book = case (decode jsonText :: Maybe Book) of
                Just v  -> v
                Nothing -> Book "" "" 0
@@ -58,4 +41,6 @@ readBook = do
 
 main :: IO ()
 main = do
+  writeBook
+  readBook
   putStrLn "OK"
--- haskell/extra/aesontets/src/Lib.hs
+++ haskell/extra/aesontets/src/Lib.hs
@@ -1,6 +1,30 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import GHC.Generics
+import Data.Aeson
+import qualified Data.Text as T
+
+data Book = Book
+  { title  :: T.Text
+  , author :: T.Text
+  , year   :: Int }
+  deriving (Show, Generic)
+
+instance FromJSON Book
+instance ToJSON Book
+
+
+
+data ErrorMesage = ErrorMesage 
+  { message   :: T.Text
+  , errorCode :: Int }
+  deriving Show
+
+instance FromJSON ErrorMesage where
+  parseJSON (Object v) = ErrorMesage <$> v .: "message" <*>  v .: "error"
+  parseJSON _ = error "123"
+
+instance ToJSON ErrorMesage where
+  toJSON (ErrorMesage msg errCode) = object
+    [ "message" .= msg
+    , "error"   .= errCode ]
